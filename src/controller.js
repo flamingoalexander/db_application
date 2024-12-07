04c87e648ab059dc2e4988d224a5bf71d82d9f15
Alexander Vokin <setvvgamer@gmail.com>
2024-12-07 20:23:03 +0800

    place fetch queries via try/catch block

--- src/controller.js
+++ src/controller.js
@@ -1,48 +1,56 @@
 export default class Controller {
     static async loadTable(tableName) {
-        const response = await fetch('http://195.133.18.211:3000/api/query', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                authorization: 'Bearer ' + localStorage.getItem('token')
-            },
-            body: JSON.stringify({
-                query: `SELECT * FROM ${tableName}`,
+        try {
+            const response = await fetch('http://195.133.18.211:3000/api/query', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: 'Bearer ' + localStorage.getItem('token')
+                },
+                body: JSON.stringify({
+                    query: `SELECT * FROM ${tableName}`,
+                })
             })
-        })
-        return await response.json()
+            return await response.json()
+        } catch (err) {
+            alert('Что то пошло не так: ' + err.message)
+        }
     }
+
     static async updateRow(row, tableName) {
-        const response = await fetch(`http://195.133.18.211:3000/api/table/${tableName}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                authorization: 'Bearer ' + localStorage.getItem('token')
-            },
-            body: JSON.stringify(row)
-        });
-        const result = await response.json();
-        if (response.ok) {
-            alert('Данные успешно сохранены.');
-        } else {
-            alert('Ошибка при сохранении данных: ' + result.error);
+        try {
+            const response = await fetch(`http://195.133.18.211:3000/api/table/${tableName}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: 'Bearer ' + localStorage.getItem('token')
+                },
+                body: JSON.stringify(row)
+            });
+            if (response.ok) {
+                alert('Данные успешно сохранены.');
+            }
+        } catch (err) {
+            alert('Ошибка при сохранении данных: ' + err.message);
+            console.error(err);
         }
     }
     static async addRow(row, tableName) {
-        const response = await fetch(`http://195.133.18.211:3000/api/table/${tableName}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                authorization: 'Bearer ' + localStorage.getItem('token')
-            },
-            body: JSON.stringify(row)
-        });
-        const result = await response.json();
-        if (response.ok) {
-            alert('Данные успешно сохранены.');
-        } else {
-            alert('Ошибка при сохранении данных: ' + result.error);
+        try {
+            const response = await fetch(`http://195.133.18.211:3000/api/table/${tableName}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    authorization: 'Bearer ' + localStorage.getItem('token')
+                },
+                body: JSON.stringify(row)
+            });
+            if (response.ok) {
+                alert('Данные успешно сохранены.');
+            }
+        } catch(err) {
+            alert('Ошибка при добавлении данных: ' + err.error);
         }
     }
 }
